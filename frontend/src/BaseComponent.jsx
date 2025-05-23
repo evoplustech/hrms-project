08b3edf6ea8a88c83651f9f2c42e2eed9a303343
Syed <Syed@edt.srv01.in>
2025-05-23 15:43:11 +0530

    testing bugss

--- frontend/src/BaseComponent.jsx
+++ frontend/src/BaseComponent.jsx
@@ -28,6 +28,7 @@ import AddHoliday from './components/Home/holiday/AddHoliday';
 import { getHolidayList } from './slices/holidaySlice';
 import currentMonthDates from '../utils/dateOfMonth';
 import CronEditor from './components/Home/cofiguration/CronEditor';
+import Biometric from './components/Home/devices/Biometric';
 
 
 
@@ -50,7 +51,7 @@ const ResetPassword = React.lazy(()=>import('./components/Login/ResetPassword'))
 const EmployeeManagement = React.lazy(()=>import('./components/Home/employee/EmployeeManagement'));
 const Dashboard = React.lazy(()=>import('./components/Home/dashboard/Dashboard'));
 const Attendance = React.lazy(()=>import('./components/Home/attendance/Attendance'));
-const Biometric = React.lazy(()=>import('./components/Home/devices/Biometric'));
+// const Biometric = React.lazy(()=>import('./components/Home/devices/Biometric'));
 const Leave = React.lazy(()=>import('./components/Home/leave/Leave'));
 const Configuration = React.lazy(()=>import('./components/Home/cofiguration/Configuration'));
 const PersonalDetailsForm = React.lazy(()=>import('./components/Home/employee/PersonalDetailsForm'));
@@ -279,7 +280,7 @@ const BaseComponent = ()=>{
       </div>
   </section>
     }
-  ])
+  ]);
 
   return (
     <RouterProvider router = {Router}></RouterProvider>

edd637c6690a0aa64eb0c4a005177a287045dfa1
Syed <Syed@edt.srv01.in>
2025-05-23 16:19:10 +0530

    testing be

--- frontend/src/components/Home/devices/BIometric.jsx
+++ frontend/src/components/Home/devices/BIometric.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
-const Biometric = ()=>{
+const BIometric = ()=>{
  return (
         // <div className="w-4/5 m-auto p-6 bg-gray-200 border-2 rounded-lg">
             <main className=" mt-36 w-full p-6  ms-6 me-2 mb-40">
@@ -11,4 +11,4 @@ const Biometric = ()=>{
     )
 }
 
-export default Biometric;
+export default BIometric;
